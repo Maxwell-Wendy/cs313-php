66ade0ba5134877cb19ab3e4ffbff22fd629b090
Wendy Maxwell <50604460+WendyMaxwell@users.noreply.github.com>
2019-09-26 13:10:58 +1000

    added jquery code

--- web/teach02javascript.js
+++ web/teach02javascript.js
@@ -6,3 +6,11 @@ function changeColor() {
 	var x = document.getElementById("color").value;
 	document.getElementById("div1").style.backgroundColor = x;
 }
+
+$(document).ready(function() {
+	$("#button2").click(function() {
+		
+		$("#div1").css("backgroundColor", $("#color").val());
+
+	})
+});
