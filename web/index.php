688e8e10bf6a94b3184a51285d5b4d83a65b4720
Wendy Maxwell <50604460+WendyMaxwell@users.noreply.github.com>
2019-09-28 16:43:04 +1000

    added content to introduction page

--- web/index.php
+++ web/index.php
@@ -16,14 +16,29 @@
 <body>
 
 	<a href="assignments.php">Access Links to Assignments</a>
+
+	<h1>Beautiful Tasmania</h1>
+
 	<div id="div_intro">
-		<p>If you ever get the chance to visit Tasmania, Australia, Cradle Mountain is a beautiful destination! This photo was taken at sunset in July of 2019. Yes, that is snow on the mountain because July is in the middle of winter in the Southern Hemisphere. </p>
+		<p>If you ever get the chance to visit Tasmania, Australia, Cradle Mountain is a beautiful destination! This photo was taken at sunset in July of 2019. Yes, that is snow on the mountain because July is in the middle of winter in the Southern Hemisphere. The following pictures are from a trip my husband and I look to Tasmania to celbrate our 30th wedding anniversary. We had a fantastic and very memorable trip!</p>
 		<img src="cradlemountain_sunset.jpeg" class="rounded img-fluid" alt="Cradle Mountain at sunset">
 	</div>
-	<img src="pademelon.jpeg" alt="Pademelon in the headlights">
-	<img src="cliff.jpeg" alt="Cliff with clouds">
-	<img src="wombat.jpeg" alt="Wombat crossing the road">
-
+	<br>
+	<div id="pademelon">
+		<p>This little pademelon was near the cabin where we stayed. A pademelon is a marsupial that looks like a small kangaroo. They are very prevalent in Tasmania.</p>
+		<img src="pademelon.jpeg" class="rounded-circle img-fluid float-left" alt="Pademelon in the headlights">
+	</div>
+	<br>
+	<div id="cliff">
+		<p>The scenery is stunning!</p>
+		<img src="cliff.jpeg" class="rounded img-fluid" alt="Cliff with clouds">
+	</div>
+	<br>
+	<div id="wombat">
+		<p>We were only there for a few days, but we saw wildlife every day. Here is a wombat taking a stroll across the road.</p>
+		<img src="wombat.jpeg" class="rounded-circle img-fluid float-right" alt="Wombat crossing the road">
+	</div>
+ 
 
 
 </body>
