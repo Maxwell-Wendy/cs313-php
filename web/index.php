69a96fec7beec881b58a96454b40b522d1e74370
Wendy Maxwell <50604460+WendyMaxwell@users.noreply.github.com>
2019-09-29 01:36:17 +1000

    fixed date/time format

--- web/index.php
+++ web/index.php
@@ -17,7 +17,7 @@
 	<h1>Beautiful Tasmania</h1>
 
 	<div id="div_intro">
-		<p>If you ever get the chance to visit Tasmania, Australia, <span id="cradle_mountain">Cradle Mountain<span id="cradle_mountain_text">This view of Cradle Mountain can be reached by driving to Lake Dove in the Cradle Mountain-Lake St Clair National Park. Gorgeous!</span></span> is a beautiful destination! This photo was taken at sunset in July of 2019. Yes, that is snow on the mountain because July is in the middle of winter in the Southern Hemisphere. The following pictures are from a trip my husband and I look to Tasmania to celbrate our 30th wedding anniversary. We had a fantastic and very memorable trip!</p>
+		<p>If you ever get the chance to visit Tasmania, Australia, <span id="cradle_mountain">Cradle Mountain<span id="cradle_mountain_text">This view of Cradle Mountain can be reached by driving to Lake Dove in the Cradle Mountain-Lake St Clair National Park. Gorgeous!</span></span> is a beautiful destination! This photo was taken at sunset in July of 2019. Yes, that is snow on the mountain because July is in the middle of winter in the Southern Hemisphere. The following pictures are from a trip my husband and I look to Tasmania to celebrate our 30th wedding anniversary. We had a fantastic and very memorable trip!</p>
 		<img src="cradlemountain_sunset.jpeg" class="rounded img-fluid" alt="Cradle Mountain at sunset">
 	</div>
 	
@@ -40,14 +40,12 @@
 		We had a great adventure! If you ever get the chance, Tasmania is a great vacation destination!
 	</div>
 
-	<?php
-
-	echo "It is currently " .date(DATE_RFC2822) . "<br>";
-
-	?>
-
-	<a href="assignments.php">Access Links to Assignments</a>
-
+	<div>
+		<?php
+		echo "It is currently " . date("l \, j M Y \, h:i:s T") . "<br>";
+		?>
+		<a href="assignments.php">Access Links to Assignments</a>
+	</div>
 
 </body>
 </html>
