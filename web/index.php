acbf74088c334c1a8fd66aa9c26b0e3553b67594
Wendy Maxwell <50604460+WendyMaxwell@users.noreply.github.com>
2019-09-28 16:03:53 +1000

    add files for Prove 02 assignment

--- web/index.php
+++ web/index.php
@@ -1,31 +1,30 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Main Page</title>
+	<title>Home Page</title>
 	<meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
-	<link rel="stylesheet" type="text/css" href="teach02styles.css">
+    <link rel="stylesheet" type="text/css" href="homepage.css">
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
-	<script type="text/javascript" src="teach02javascript.js"></script>
+    <script type="text/javascript" src="homepage.js"></script>
 
 </head>
 
 <body>
-	<p>Hello World!></p>
 
-	<a href="teach02.html">Teach 02 Assignment</a>
+	<a href="assignments.php">Access Links to Assignments</a>
+	<div id="div_intro">
+		<p>If you ever get the chance to visit Tasmania, Australia, Cradle Mountain is a beautiful destination! This photo was taken at sunset in July of 2019. Yes, that is snow on the mountain because July is in the middle of winter in the Southern Hemisphere. </p>
+		<img src="cradlemountain_sunset.jpeg" class="rounded img-fluid" alt="Cradle Mountain at sunset">
+	</div>
+	<img src="pademelon.jpeg" alt="Pademelon in the headlights">
+	<img src="cliff.jpeg" alt="Cliff with clouds">
+	<img src="wombat.jpeg" alt="Wombat crossing the road">
 
 
 
-
-
-<?php
-
-  phpinfo();
-
-?>
 </body>
 </html>
