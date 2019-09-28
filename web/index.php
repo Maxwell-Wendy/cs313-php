9f4912c0ebfa31b0aefde8860d5df009f039a9d8
Wendy Maxwell <50604460+WendyMaxwell@users.noreply.github.com>
2019-09-28 12:58:23 +1000

    modified index page

--- web/index.php
+++ web/index.php
@@ -1,5 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Main Page</title>
+	<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="teach02styles.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+	<script type="text/javascript" src="teach02javascript.js"></script>
+
+</head>
+
+<body>
+	<p>Hello World!></p>
+
+	<a href="teach02.html">Teach 02 Assignment</a>
+
+
+
+
+
 <?php
 
   phpinfo();
 
 ?>
+</body>
+</html>
