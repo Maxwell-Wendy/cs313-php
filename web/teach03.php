7c57f336786ba171134f59f97395b361af08ba21
Wendy Maxwell <50604460+WendyMaxwell@users.noreply.github.com>
2019-10-02 11:02:10 +1000

    added teach03

---
+++ web/teach03.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<form action="name.php" method="post">
+Name: <input="text" name="name"><br>
+Email: <input="text" name="email"><br>
+Major: <input type="radio" name="major" value="Computer Science">
+<input type="radio" name="major" value="Web Design and Development">
+<input type="radio" name="major" value="Computer Information Technology">
+<input type="radio" name="major" value="Computer Engineering">
+Comments: <textarea name="comment" rows="5" cols="40"></textarea>
