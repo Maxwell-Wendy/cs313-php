3a5aec6ddb75e10cead741d6042bbf2d912ca499
Wendy Maxwell <50604460+WendyMaxwell@users.noreply.github.com>
2019-10-02 11:31:12 +1000

    modified

--- web/name.php
+++ web/name.php
@@ -6,9 +6,16 @@ echo "<br>";
 $mail = $_POST["email"];
 
 echo "<a href=\"mailto:".$mail."\">Email Address</a>";
-
+echo "<br>";
 echo $_POST["major"];
-
+echo "<br>";
 echo $_POST["comment"];
+echo "<br>";
+echo "Your continents are: <br>"
+$continents = $_POST["continent[]"]
+
+foreach ($continents as $value) {
+	echo "$value <br>";
+}
 
 ?>
--- web/teach03.php
+++ web/teach03.php
@@ -12,6 +12,15 @@ Major: <br>
 <input type="radio" name="major" value="Computer Engineering">Computer Engineering<br>
 Comments: <textarea name="comment" rows="5" cols="40"></textarea>
 
+Continents Visited: <br>
+<input type="checkbox" name="continent[]" value="north america">North America<br>
+<input type="checkbox" name="continent[]" value="south america">South America<br>
+<input type="checkbox" name="continent[]" value="europe">Europe<br>
+<input type="checkbox" name="continent[]" value="australia">Australia<br>
+<input type="checkbox" name="continent[]" value="asia">Asia<br>
+<input type="checkbox" name="continent[]" value="africa">Africa<br>
+<input type="checkbox" name="continent[]" value="antartica">Antartica<br>
+
 <input type="submit">
 </form>
 
