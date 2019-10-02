364601e627dcfb383ae5c09d38c968593f067b82
Wendy Maxwell <50604460+WendyMaxwell@users.noreply.github.com>
2019-10-02 11:06:57 +1000

    modified

--- web/teach03.php
+++ web/teach03.php
@@ -5,8 +5,11 @@
 <form action="name.php" method="post">
 Name: <input="text" name="name"><br>
 Email: <input="text" name="email"><br>
-Major: <input type="radio" name="major" value="Computer Science">
-<input type="radio" name="major" value="Web Design and Development">
-<input type="radio" name="major" value="Computer Information Technology">
-<input type="radio" name="major" value="Computer Engineering">
+Major: <input type="radio" name="major" value="Computer Science">Computer Science<br>
+<input type="radio" name="major" value="Web Design and Development">Web Design and Development<br>
+<input type="radio" name="major" value="Computer Information Technology">Computer Information Technology<br>
+<input type="radio" name="major" value="Computer Engineering">Computer Engineering<br>
 Comments: <textarea name="comment" rows="5" cols="40"></textarea>
+
+</body>
+</html>
