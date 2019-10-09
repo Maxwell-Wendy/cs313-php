5055f734af0f941033a8f49c91a045d30183acac
Wendy Maxwell <50604460+WendyMaxwell@users.noreply.github.com>
2019-10-09 22:55:02 +1100

    added checkout and confirm pages

--- web/03prove_checkout.php
+++ web/03prove_checkout.php
@@ -16,22 +16,22 @@ if (!isset($_SESSION['cart'])) {
 </head>
 <body>
 	<a href="03prove_cart.php">Back to Cart</a>
-
+	<h1>Enter your Shipping Information</h1>
 	<div class="address">
 		<form action="03prove_confirm.php" method="post">
 			<label for="name">Full Name</label>
-			<input type="text" id="name" name="name" placeholder="John Doe">
+			<input type="text" id="name" name="name" placeholder="John Doe"><br>
 			<label for="street">Street Address</label>
-			<input type="text" id="street" name="street" placeholder="123 Any St.">
+			<input type="text" id="street" name="street" placeholder="123 Any St."><br>
 			<label for="city">City</label>
 			<input type="text" id="city" name="city" placeholder="Your City">
 			<label for="state">State</label>
 			<input type="text" id="state" name="state" placeholder="Your State">
 			<label for="zip">Zip Code</label>
-			<input type="text" id="zip" name="zip" placeholder="00000">
+			<input type="text" id="zip" name="zip" placeholder="00000"><br>
 			<label for="email">Email</label>
-			<input type="text" id="email" name="email" placeholder="johndoe@example.com">
-			<input type="submit" name="submit" value="Go to Confirmation Page">
+			<input type="text" id="email" name="email" placeholder="johndoe@example.com"><br>
+			<input type="submit" name="submit" value="Place your Order">
 		</form>
 		<form action="03prove_cart.php" method="post">
 			<input type="submit" name="submit" value="Return to Cart">
--- web/03prove_confirm.php
+++ web/03prove_confirm.php
@@ -17,5 +17,33 @@ if (!isset($_SESSION['cart'])) {
 <body>
 	<a href="03prove_checkout.php">Back to Checkout</a>
 
+	<h2>Your order details</h2>
+	<div class="confirm">
+		<?php
+			$name = $_POST["name"];
+			$street = $_POST["street"];
+			$city = $_POST["city"];
+			$state = $_POST["state"];
+			$zip = $_POST["zip"];
+			$email = $_POST["email"];
+
+			echo "You have ordered: <br>";
+
+			if(!empty($_SESSION['cart'])) {
+				foreach ($_SESSION['cart'] as $key => $value) {
+					echo $value . "<br>";
+				}
+			}
+
+			echo "It will be shipped to: <br>";
+			echo $name . "<br>" . $street . "<br>";
+			echo $city . ", " . $state . " " . $zip . "<br>";
+
+			echo "Your confirmation email will be sent to " . $email . "<br>";
+			echo "Thank you for your order!";
+
+		?>
+	</div>
+
 </body>
 </html>
