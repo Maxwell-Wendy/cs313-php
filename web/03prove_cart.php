84906cb6e85c8ae2d4a9223fb53e808c10c6101e
Wendy Maxwell <50604460+WendyMaxwell@users.noreply.github.com>
2019-10-09 21:01:52 +1100

    modified code

--- web/03prove_cart.php
+++ web/03prove_cart.php
@@ -10,7 +10,7 @@ if (!isset($_SESSION['cart'])) {
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Browse Items</title>
+	<title>Cart</title>
 	<script type="text/javascript" src="03prove.js"></script>
 </head>
 <body>
@@ -25,7 +25,7 @@ if (!isset($_SESSION['cart'])) {
  	foreach ($_SESSION['cart'] as $key => $value) {
  		echo "You have added " . $value . " to your cart.<br>";
 ?>
-    <p>This is a test to print the name: <?php echo $_SESSION['cart'][$value]?></p>
+    <p>This is a test to print the name: <?php echo $_SESSION['cart'][0]?></p>
         <form action="<?php echo htmlentities($_SERVER['PHP_SELF']); ?>" method="post">
 			<input type="hidden" name="name" value="<?php echo $_SESSION['cart'][$value]?>">
 			<input type="submit" name="submit" value="Remove Item">
