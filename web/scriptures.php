03e2424062302aed7dbe8aecfa3300e1eac5549a
Wendy Maxwell <50604460+WendyMaxwell@users.noreply.github.com>
2019-10-14 21:14:07 +1100

    modified

--- web/scriptures.php
+++ web/scriptures.php
@@ -11,12 +11,27 @@
 	<?php
 	$db = parse_url(getenv("DATABASE_URL"));
 	$db["path"] = ltrim($db["path"], "/");
-	$result = pg_query($db, "SELECT book FROM scriptures WHERE scriptures.id =1");
-	echo $db;
+
+	$dbconn = pg_connect($db)
+		or die('Could not connect:' . pg_last_error());
+
+	$query = 'SELECT * FROM scriptures';
+
+	$result = pg_query($query) or die ('Error message: ' . pg_last_error());
+
+
+	while ($row = pg_fetch_row($result)) {
+		var_dump($row);
+	}
+
+	pg_free_result($result);
+	pg_close($dbconn);
+	//$result = pg_query($db, "SELECT book FROM scriptures WHERE scriptures.id =1");
+	//echo $db;
 	echo "<br>Wendy <br>";
-	echo $db["path"];
+	//echo $db["path"];
 	echo "<br>Maxwell <br>";
-	var_dump($result);
+	//var_dump($result);
 	?>
 
 </body>
