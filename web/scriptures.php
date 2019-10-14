c5c4603b62060144ac2eafbb6253659f15062edd
Wendy Maxwell <50604460+WendyMaxwell@users.noreply.github.com>
2019-10-14 18:22:11 +1100

    modified

--- web/scriptures.php
+++ web/scriptures.php
@@ -12,12 +12,11 @@
 	$db = parse_url(getenv("DATABASE_URL"));
 	$db["path"] = ltrim($db["path"], "/");
 	$result = pg_query($db, "SELECT book FROM scriptures WHERE scriptures.id =1");
-	$book = scriptures.book;
 	echo $db;
 	echo "<br>Wendy <br>";
 	echo $db["path"];
 	echo "<br>Maxwell <br>";
-	echo $book;
+	echo $result;
 	?>
 
 </body>
