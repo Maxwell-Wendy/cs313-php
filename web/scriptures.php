fb773e109a78af2a0e961fecd23d6d6745e1532e
Wendy Maxwell <50604460+WendyMaxwell@users.noreply.github.com>
2019-10-14 21:40:46 +1100

    modified

--- web/scriptures.php
+++ web/scriptures.php
@@ -9,39 +9,53 @@
 	<h1>Scripture Resources</h1>
 
 	<?php
-	$db = parse_url(getenv("DATABASE_URL"));
-	//$db["path"] = ltrim($db["path"], "/");
+	try{
 
-	//$dbconn = pg_connect($db)
-		//or die('Could not connect:' . pg_last_error());
+		$db = parse_url(getenv("DATABASE_URL"));
+		//$db["path"] = ltrim($db["path"], "/");
 
-	$pdo = new PDO("pgsql:" . sprintf(
-	    "host=%s;port=%s;user=%s;password=%s;dbname=%s",
-	    $db["host"],
-	    $db["port"],
-	    $db["user"],
-	    $db["pass"],
-	    ltrim($db["path"], "/")
-	))
-		or die('Could not connect: ' . pg_last_error());
+		//$dbconn = pg_connect($db)
+			//or die('Could not connect:' . pg_last_error());
 
-	$query = 'SELECT * FROM book';
+		$pdo = new PDO("pgsql:" . sprintf(
+		    "host=%s;port=%s;user=%s;password=%s;dbname=%s",
+		    $db["host"],
+		    $db["port"],
+		    $db["user"],
+		    $db["pass"],
+		    ltrim($db["path"], "/")
+		))
+			or die('Could not connect: ' . pg_last_error());
 
-	$result = pg_query($query) or die ('Error message: ' . pg_last_error());
+		/*$query = 'SELECT * FROM book';
+
+		$result = pg_query($query) or die ('Error message: ' . pg_last_error());
 
 
-	while ($row = pg_fetch_row($result)) {
-		var_dump($row);
+		while ($row = pg_fetch_row($result)) {
+			var_dump($row);
+		}
+
+		pg_free_result($result);
+		pg_close($dbconn);
+		//$result = pg_query($db, "SELECT book FROM scriptures WHERE scriptures.id =1");
+		//echo $db;
+		echo "<br>Wendy <br>";
+		//echo $db["path"];
+		echo "<br>Maxwell <br>";
+		//var_dump($result);*/
+
+		$sql = 'SELECT * FROM scriptures';
+
+		foreach ($pdo->query($sql) as $row) {
+			var_dump($row);
+		}
+
+		$pdo = null;
+
+	} catch (PDOExcetion $e) {
+		die("Error message: " . $e->getMessage());
 	}
-
-	pg_free_result($result);
-	pg_close($dbconn);
-	//$result = pg_query($db, "SELECT book FROM scriptures WHERE scriptures.id =1");
-	//echo $db;
-	echo "<br>Wendy <br>";
-	//echo $db["path"];
-	echo "<br>Maxwell <br>";
-	//var_dump($result);
 	?>
 
 </body>
