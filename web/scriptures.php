c541fa1d59fd9f6c639c8c898c4ce13216881da0
Wendy Maxwell <50604460+WendyMaxwell@users.noreply.github.com>
2019-10-14 22:20:49 +1100

    modified

--- web/scriptures.php
+++ web/scriptures.php
@@ -9,13 +9,8 @@
 	<h1>Scripture Resources</h1>
 
 	<?php
-	try{
-
+	try {
 		$db = parse_url(getenv("DATABASE_URL"));
-		//$db["path"] = ltrim($db["path"], "/");
-
-		//$dbconn = pg_connect($db)
-			//or die('Could not connect:' . pg_last_error());
 
 		$pdo = new PDO("pgsql:" . sprintf(
 		    "host=%s;port=%s;user=%s;password=%s;dbname=%s",
@@ -27,33 +22,12 @@
 		))
 			or die('Could not connect: ' . pg_last_error());
 
-		/*$query = 'SELECT * FROM book';
-
-		$result = pg_query($query) or die ('Error message: ' . pg_last_error());
-
-
-		while ($row = pg_fetch_row($result)) {
-			var_dump($row);
-		}
-
-		pg_free_result($result);
-		pg_close($dbconn);
-		//$result = pg_query($db, "SELECT book FROM scriptures WHERE scriptures.id =1");
-		//echo $db;
-		echo "<br>Wendy <br>";
-		//echo $db["path"];
-		echo "<br>Maxwell <br>";
-		//var_dump($result);*/
-
 		$sql = 'SELECT * FROM scriptures';
 
 		foreach ($pdo->query($sql) as $row) {
 
 			echo "<b>" . $row['book'] . " " . $row['chapter'] . ":" . $row['verse'] . " - </b>";
-			//echo $row['chapter'] . ":";
-			//echo $row['verse'] . " - ";
-			echo $row['content'] . "<br><br>";
-			//var_dump($row);
+			echo "\"" . $row['content'] . "\"<br><br>";
 		}
 
 		$pdo = null;
