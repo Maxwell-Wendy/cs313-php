445977ddb79c53812dea2b40326d77a25358a3e4
Wendy Maxwell <50604460+WendyMaxwell@users.noreply.github.com>
2019-10-14 17:32:38 +1100

    modified

--- web/scriptures.php
+++ web/scriptures.php
@@ -11,7 +11,7 @@
 	<?php
 	$db = parse_url(getenv("DATABASE_URL"));
 	$db["path"] = ltrim($db["path"], "/");
-	$result = pg_query($db, "SELECT * FROM scriptures");
+	$result = pg_query("SELECT * FROM scriptures");
 	echo $db;
 	echo $db["path"];
 	echo $result;
