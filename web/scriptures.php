0a7125708b6ecb404c5ab202af2bcf1dc5823743
Wendy Maxwell <50604460+WendyMaxwell@users.noreply.github.com>
2019-10-14 18:04:01 +1100

    modified

--- web/scriptures.php
+++ web/scriptures.php
@@ -11,9 +11,11 @@
 	<?php
 	$db = parse_url(getenv("DATABASE_URL"));
 	$db["path"] = ltrim($db["path"], "/");
-	$result = pg_query("SELECT * FROM scriptures");
+	$result = pg_query($db, "SELECT * FROM scriptures WHERE scriptures.id =1");
 	echo $db;
+	echo "<br>Wendy <br>";
 	echo $db["path"];
+	echo "<br>Maxwell <br>";
 	echo $result;
 	?>
 
