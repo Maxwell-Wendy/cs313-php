131129855b5aa193e445a9c95c3c4446633d5b92
Wendy Maxwell <50604460+WendyMaxwell@users.noreply.github.com>
2019-10-14 16:38:19 +1100

    added scriptures.php

---
+++ web/scriptures.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<title>Scriptures</title>
+</head>
+
+<body>
+	<h1>Scripture Resources</h1>
+
+	<?php
+	$db = parse_url(getenv("DATABASE_URL"));
+	$db["path"] = ltrim($db["path"], "/");
+	$result = pg_query($db, "SELECT * FROM scriptures");
+	echo $result;
+	?>
+
+</body>
+</html>
+
