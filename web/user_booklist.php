cde8af663441268cff2a1710e48021474401071d
Wendy Maxwell <50604460+WendyMaxwell@users.noreply.github.com>
2019-10-19 16:30:04 +1100

    modifying

--- web/user_booklist.php
+++ web/user_booklist.php
@@ -35,7 +35,7 @@ $db = get_db();
 			
 
 			foreach ($db->query($sql) as $row) {
-				$url = "book_details.php?bookid=" . $row['book_id'] . "&username=" . $_SESSION['user'];
+				$url = "book_details.php?bookid=" . $row['book_id'] . "&userid=" . $row['user_id'];
 
 				$first_name = $row['first_name'];
 				$last_name = $row['last_name'];
