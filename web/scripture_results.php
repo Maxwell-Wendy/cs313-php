a50757aff3e2b5057bc13d99c4ec19eb0f7be9e3
Wendy Maxwell <50604460+WendyMaxwell@users.noreply.github.com>
2019-10-23 17:18:39 +1100

    fixing

--- web/scripture_results.php
+++ web/scripture_results.php
@@ -29,4 +29,23 @@ $db = get_db();
 		}
 	}
 ?>
+<?php
+		if (isset($_POST['book'])) {
+
+			$book = $_POST['book'];
+
+			$sql = "SELECT id, book, chapter, verse FROM Scriptures WHERE scriptures.book = '$book'";
+
+			foreach ($db->query($sql) as $row) {
+				$id = $row['id'];
+				$b = $row['book'];
+				$ch = $row['chapter'];
+				$v = $row['verse'];
+
+				$url = "scripture_results.php?id=$id";
+
+				echo "<b><a href=\"$url\">$b $ch:$v</a></b><br/>";
+			}
+		}
+	?>
 </body>
