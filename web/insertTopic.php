14ab0b77a20733a7c0ea71b7de0bd4b9f194b85b
Wendy Maxwell <50604460+WendyMaxwell@users.noreply.github.com>
2019-10-23 21:45:35 +1100

    fixing

--- web/insertTopic.php
+++ web/insertTopic.php
@@ -58,7 +58,7 @@ $db = get_db();
       }
       
       // finally, redirect them to a new page to actually show the topics
-      //header("Location: showTopics.php");
+      header("Location: showTopics.php");
       
       die(); // we always include a die after redirects. In this case, there would be no
              // harm if the user got the rest of the page, because there is nothing else
