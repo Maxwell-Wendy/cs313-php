42bf6550f95a10da7b73b390b474dcfaa0e4b1f0
Wendy Maxwell <50604460+WendyMaxwell@users.noreply.github.com>
2019-10-23 16:40:29 +1100

    fixed

--- web/scripture_results.php
+++ web/scripture_results.php
@@ -11,11 +11,27 @@ $db = get_db();
 	<a href="scriptures.php">Back to Scriptures start page</a>
 	<h1>Scripture Resources</h1>
 <?php
+
+	if (isset($_GET['id'])) {
+	
+		$id = $_GET['id'];
+
+	/*$sql = "SELECT * FROM scriptures WHERE scriptures.id = '$id'";
 ​
-	foreach ($db->query("SELECT * FROM Scriptures WHERE Scriptures.id =" . $_GET['id']) as $row) {
+	foreach ($db->query($sql) as $row) {
 		echo "<b>" . $row['book'] . " " . $row['chapter'] . ":" . $row['verse'] . " - </b>";
             echo '"' . $row['content'] . '"<br><br>';
+	}*/
+
+		$sql = "SELECT book, chapter, verse FROM scriptures WHERE scriptures.id = '$id'";
+
+		foreach ($db->query($sql) as $row) {
+			$url = "scripture_results.php?id=" . $row['id'];
+
+			echo "<b><a href=\"$url\">" . $row['book'] . " " . $row['chapter'] . ":" . $row['verse'] . "</a></b><br/>";
+
+			echo $row['content'];
+		}
 	}
-​
 ?>
 </body>
--- web/scriptures.php
+++ web/scriptures.php
@@ -34,7 +34,7 @@ $db = get_db();
 
 	?>
 
-	<form name="search" action="scriptures.php" method="post">
+	<form name="search" action="scripture_results.php" method="post">
 		Search Book: <input type="text" name="book" />
 		<input type="submit" value="Search"><br/>
 	</form>
