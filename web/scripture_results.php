428bb38a9ab1b11d5a3a22a70d01c6e70a4a4721
Wendy Maxwell <50604460+WendyMaxwell@users.noreply.github.com>
2019-10-23 17:14:54 +1100

    fixing

--- web/scripture_results.php
+++ web/scripture_results.php
@@ -9,19 +9,12 @@ $db = get_db();
 </head>
 <body>
 	<a href="scriptures.php">Back to Scriptures start page</a>
-	<h1>Scripture Resources</h1>
+	<h1>Scripture Details</h1>
 <?php
 
 	if (isset($_GET['id'])) {
 	
 		$id = $_GET['id'];
-
-	/*$sql = "SELECT * FROM scriptures WHERE scriptures.id = '$id'";
-​
-	foreach ($db->query($sql) as $row) {
-		echo "<b>" . $row['book'] . " " . $row['chapter'] . ":" . $row['verse'] . " - </b>";
-            echo '"' . $row['content'] . '"<br><br>';
-	}*/
 
 		$sql = "SELECT book, chapter, verse, content FROM scriptures WHERE scriptures.id = '$id'";
 
@@ -31,11 +24,8 @@ $db = get_db();
 			$v = $row['verse'];
 			$c = $row['content'];
 
-			//echo "<b>" . $row['book'] . " " . $row['chapter'] . ":" . $row['verse'] . "</a></b> " . $row['content'] . "<br/>";
-
 			echo "<b>$b $ch:$v</b> $c<br>";
 
-			//echo $row['content'] "<br>";
 		}
 	}
 ?>
--- web/scriptures.php
+++ web/scriptures.php
@@ -6,21 +6,14 @@ $db = get_db();
 ?>
 <!DOCTYPE html>
 <html>
-
 <head>
 	<title>Scriptures</title>
 </head>
-
 <body>
 	<a href="assignments.php">Back to Assignments Page</a>
 	<h1>Scripture Resources</h1>
 
 	<?php
-	//if (isset($_POST['book'])) {
-
-		//$book = $_POST['book'];
-
-		//$sql = "SELECT * FROM Scriptures WHERE Scriptures.book = '$book'";
 		$sql = 'SELECT id, book, chapter, verse FROM scriptures';
 
 		foreach ($db->query($sql) as $row) {
@@ -32,9 +25,6 @@ $db = get_db();
 			$url = "scripture_results.php?id=$id";
 
 			echo "<b><a href=\"$url\">$b $ch:$v</a></b><br/>";
-
-			//echo "<b>" . $row['book'] . " " . $row['chapter'] . ":" . $row['verse'] . " - </b>";
-			//echo "\"" . $row['content'] . "\"<br><br>";
 		}
 
 	?>
@@ -43,6 +33,25 @@ $db = get_db();
 		Search Book: <input type="text" name="book" />
 		<input type="submit" value="Search"><br/>
 	</form>
+
+	<?php
+		if (isset($_POST['book'])) {
+
+		$book = $_POST['book'];
+
+		$sql = "SELECT id, book, chapter, verse FROM Scriptures WHERE scriptures.book = '$book'";
+
+		foreach ($db->query($sql) as $row) {
+			$id = $row['id'];
+			$b = $row['book'];
+			$ch = $row['chapter'];
+			$v = $row['verse'];
+
+			$url = "scripture_results.php?id=$id";
+
+			echo "<b><a href=\"$url\">$b $ch:$v</a></b><br/>";
+		}
+	?>
 
 </body>
 </html>
