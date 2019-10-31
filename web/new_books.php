14bb370d6dc8e62238c4c56372487521ad1dd8c8
Wendy Maxwell <50604460+WendyMaxwell@users.noreply.github.com>
2019-10-31 21:01:03 +1100

    fixing

--- web/new_books.php
+++ web/new_books.php
@@ -29,6 +29,44 @@ else
 
 <body>
 	<a href="bookcatalogue.php">Back to Book Catalogue Page</a>
+	<?php
+		//if (isset($_POST['submit_list'])) {
+
+			echo "<h2>Click book for more details</h2>";
+
+			if (isset($_SESSION['user'])) {
+				$username = $_SESSION['user'];
+			}
+			else {
+				header("Location: signin.php");
+				die(); 
+			}
+
+			//echo "<p>Your username is $username.<p>";
+
+			$sql = "SELECT author.name AS name, 
+				book.title AS title, 
+				book_user.book_id AS book_id, 
+				book_user.user_id AS user_id 
+			FROM book_user 
+			INNER JOIN book ON book_user.book_id = book.id 
+			INNER JOIN user_info ON book_user.user_id = user_info.id 
+			INNER JOIN author ON book.author_id = author.id 
+			WHERE user_info.username = '$username'
+			ORDER BY name";
+
+			
+
+			foreach ($db->query($sql) as $row) {
+				$url = "book_details.php?bookid=" . $row['book_id'] . "&userid=" . $row['user_id'];
+
+				$name = $row['name'];
+				$title = $row['title'];
+
+				echo "<a href=\"$url\">$name, <i>$title</i></a><br>";
+			}
+		//}
+		?>
 
 	<h1>Add a book from our ever-growing catalogue to your personal catalogue</h1>
 	<form name="add_existing_book" action="insert_book.php" method="POST">
