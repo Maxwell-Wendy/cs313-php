a1b87a7ec0873831c6c23d8cfc3ff57384fd17ae
Wendy Maxwell <50604460+WendyMaxwell@users.noreply.github.com>
2019-11-02 17:37:03 +1100

    modifying

--- web/new_books.php
+++ web/new_books.php
@@ -55,16 +55,7 @@ $db = get_db();
 			<select name="title">
 				<option> </option>
 				<?php
-
-					//$sql = "SELECT title FROM book ORDER BY title";
-					$sql = "SELECT book.title AS title 
-						FROM book_user 
-						INNER JOIN user_info ON book_user.user_id = user_info.id 
-						INNER JOIN book ON book_user.book_id = book.id 
-						INNER JOIN author ON book.author_id = author.id
-						INNER JOIN genre ON book.genre_id = genre.id
-						WHERE user_info.username <> '$username'
-						ORDER BY title";
+					$sql = "SELECT title FROM book ORDER BY title";
 
 					foreach ($db->query($sql) as $row) {
 						$book = $row['title'];
